b5fb7e42f639bd74ab515ffe942007da455d7bd0
zhiwi1 <zhyuliuk.ivan@gmail.com>
2021-09-21 20:00:04 +0300

    add tests

--- src/main/java/by/zhyuliuk/task/utils/StringUtils.java
+++ src/main/java/by/zhyuliuk/task/utils/StringUtils.java
@@ -3,12 +3,19 @@ package by.zhyuliuk.task.utils;
 import org.apache.commons.lang3.math.NumberUtils;
 
 public final class StringUtils {
-//todo exceptions
-    private StringUtils() {
+
+    private StringUtils() throws IllegalAccessException {
+        throw new IllegalAccessException();
     }
 
-    public static boolean isPositiveNumber(String str) {
-        return NumberUtils.isCreatable(str) && NumberUtils.createInteger(str) > 0;
+    public static boolean isPositiveNumber(String str) throws NumberFormatException {
+        boolean result ;
+        if (NumberUtils.isCreatable(str)) {
+            result = NumberUtils.createInteger(str) > 0;
+        } else {
+            throw new NumberFormatException();
+        }
+        return result;
     }
 
 
--- src/test/java/by/zhyuliuk/task/utils/StringUtilsTest.java
+++ src/test/java/by/zhyuliuk/task/utils/StringUtilsTest.java
@@ -2,13 +2,29 @@ package by.zhyuliuk.task.utils;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringUtilsTest {
-//todo first
-    @Test
-    public void isPositiveNumberTest() {
+
+    @Test()
+    public void isPositiveNumberTrueTest() {
         boolean actual = StringUtils.isPositiveNumber("225");
         assertTrue(actual);
+    }
+
+    @Test
+    public void isPositiveNumberFalseTest() {
+        boolean actual = StringUtils.isPositiveNumber("-123");
+        assertFalse(actual);
+    }
+
+
+    @Test
+    public void isPositiveNumberExceptionTest() {
+        assertThrows(NumberFormatException.class, () -> {
+            StringUtils.isPositiveNumber("dlsasad");
+        });
     }
 }
